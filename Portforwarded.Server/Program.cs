6c0f198705ddd5aa00bf46d1c19c2af55b60eff8
NoLifeKing <webmaster@itssimple.se>
2021-03-27 18:34:08 +0100

    feat: First viable version

--- Portforwarded.Server/Program.cs
+++ Portforwarded.Server/Program.cs
@@ -4,28 +4,48 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Portforwarded.Server
 {
     class Program
     {
+        public static volatile bool SENDING_CTRL_C_TO_CHILD = false;
+
         static IConfiguration Configuration;
+        static Process process;
+        static Open.Nat.NatDevice UPnPDevice;
+        public static CancellationTokenSource CancellationTokenSource { get; private set; }
+
+        static CancellationToken cancellationToken;
         async static Task<int> Main(string[] args)
         {
+            CancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = CancellationTokenSource.Token;
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
             LogLine("Launching Portforwarded.Server, loading configuration");
 
-            var config = LoadAndValidateConfig(args);
-            if (!config.valid)
+            var (valid, errors) = LoadAndValidateConfig(args);
+            if (!valid)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 LogLine("Configuration not valid, reasons stated below:", ConsoleColor.Red);
-                config.errors.ForEach(l => LogLine(l, ConsoleColor.Red));
+                errors.ForEach(l => LogLine(l, ConsoleColor.Red));
 
                 return -1;
             }
 
-            if (!ExecutableExists())
+            UPnPDevice = await GetUPnPDevice();
+            if (UPnPDevice == null)
+            {
+                LogLine("No UPnP device found, exiting.", ConsoleColor.Red);
+                return -1;
+            }
+
+            if (!WorkingDirectoryExists())
             {
                 LogLine("Configured executable or directory does not exist, please check the path and file variables", ConsoleColor.Red);
                 return -1;
@@ -33,7 +53,7 @@ namespace Portforwarded.Server
 
             try
             {
-                SetupPortforwarding();
+                await SetupPortforwarding();
                 await LaunchProcess();
             }
             catch (Exception ex)
@@ -41,11 +61,27 @@ namespace Portforwarded.Server
                 LogLine(ex.ToString(), ConsoleColor.Red);
             } finally
             {
-                TeardownPortforwarding();
+                CancellationTokenSource.Dispose();
+                await TeardownPortforwarding();
             }
 
-            Console.ReadLine();
             return 0;
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            if (process != null && !process.HasExited)
+            {
+                CancellationTokenSource.CancelAfter(5_000);
+
+                process.CancelOutputRead();
+                process.CancelErrorRead();
+                process.CloseMainWindow();
+                process.Kill(true);
+            }
+
+            LogLine("Exiting process", ConsoleColor.Red);
+            e.Cancel = true;
         }
 
         private static async Task LaunchProcess()
@@ -56,7 +92,9 @@ namespace Portforwarded.Server
                 LogLine($"Using parameters: {Configuration["executable:parameters"]}");
             }
 
-            using (var process = new Process())
+            LogLine();
+
+            using (process = new Process())
             {
                 process.StartInfo = new ProcessStartInfo
                 {
@@ -74,6 +112,9 @@ namespace Portforwarded.Server
 
                 process.Start();
 
+                process.BeginErrorReadLine();
+                process.BeginOutputReadLine();
+
                 await process.WaitForExitAsync();
             }
         }
@@ -88,29 +129,98 @@ namespace Portforwarded.Server
             LogLine(e.Data);
         }
 
-        private static void SetupPortforwarding()
+        /// <summary>
+        /// Fetches a UPnP device from the network
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<Open.Nat.NatDevice> GetUPnPDevice()
+        {
+            var discoverer = new Open.Nat.NatDiscoverer();
+            return await discoverer.DiscoverDeviceAsync(Open.Nat.PortMapper.Upnp, CancellationTokenSource);
+        }
+
+        /// <summary>
+        /// Used to get the local IP address, that is on the same network as the UPnP device
+        /// </summary>
+        /// <param name="upnpDevice"></param>
+        /// <returns></returns>
+        private static IPAddress GetLocalIP(Open.Nat.NatDevice upnpDevice)
+        {
+            var hostEntries = Dns.GetHostEntry(Dns.GetHostName());
+            var ipAddresses = hostEntries.AddressList.Select(i => i.ToString());
+
+            var upnpDeviceEndpoint = upnpDevice.ToString().Split("\n").FirstOrDefault(d => d.Contains("EndPoint"));
+
+            var endpointAddress = string.Join('.', upnpDeviceEndpoint.Replace("EndPoint: ", "").Trim().Split('.').Take(2)) + ".";
+            var matchingIPAddress = ipAddresses.FirstOrDefault(a => a.StartsWith(endpointAddress));
+
+            if (string.IsNullOrWhiteSpace(matchingIPAddress)) return null;
+
+            return IPAddress.Parse(matchingIPAddress);
+        }
+
+        private static async Task SetupPortforwarding()
         {
             LogLine("Setting up portforwarding", ConsoleColor.Green);
+            LogLine();
+
+            var forwardMapping = GetForwardMappingFromConfig();
+            foreach (var map in forwardMapping)
+            {
+                //await UPnPDevice.DeletePortMapAsync(map);
+                await UPnPDevice.CreatePortMapAsync(map);
+
+                LogLine($"Created map for IP: {map.PrivateIP} (Local port: {map.PrivatePort}, Public port: {map.PublicPort}, Protocol: {map.Protocol})");
+            }
+
+            await GetCurrentMappings();
         }
 
-        private static void TeardownPortforwarding()
+        private static async Task TeardownPortforwarding()
         {
             LogLine("Tearing down portforwarding", ConsoleColor.Green);
+            LogLine();
+
+            var forwardMapping = GetForwardMappingFromConfig();
+            foreach (var map in forwardMapping)
+            {
+                await UPnPDevice.DeletePortMapAsync(map);
+
+                LogLine($"Removed map for IP: {map.PrivateIP} (Local port: {map.PrivatePort}, Public port: {map.PublicPort}, Protocol: {map.Protocol})");
+            }
+
+            await GetCurrentMappings();
         }
 
-        private static bool ExecutableExists()
+        private static async Task GetCurrentMappings()
         {
-            if (!Directory.Exists(Configuration["executable:workingdirectory"]))
-            {
-                return false;
-            }
+            var currentMappings = await UPnPDevice.GetAllMappingsAsync();
 
-            if (!File.Exists(Path.Combine(Configuration["executable:workingdirectory"], Configuration["executable:file"])))
-            {
-                return false;
-            }
+            LogLine();
 
-            return true;
+            LogLine("Currently mapped ports and IP addresses", ConsoleColor.Cyan);
+            LogLine();
+            foreach (var map in currentMappings)
+            {
+                LogLine($"Map: {map.PrivateIP}->{map.PublicIP} ({map.PrivatePort}->{map.PublicPort}) Expiration: {map.Expiration}");
+            }
+        }
+
+        private static IEnumerable<Open.Nat.Mapping> GetForwardMappingFromConfig()
+        {
+            return Configuration.GetSection("upnp").Get<List<ForwardMapping>>().Select(GetMapping);
+        }
+
+        private static Open.Nat.Mapping GetMapping(ForwardMapping map)
+        {
+            var localIp = string.IsNullOrWhiteSpace(map.LocalIPAddress) ? UPnPDevice.LocalAddress : IPAddress.Parse(map.LocalIPAddress);
+            var mapping = new Open.Nat.Mapping(map.Protocol, localIp, map.LocalPort, map.PublicPort, 0, $"Portforwarded.Server ({map.LocalPort}->{map.PublicPort})");
+            return mapping;
+        }
+
+        private static bool WorkingDirectoryExists()
+        {
+            return Directory.Exists(Configuration["executable:workingdirectory"]);
         }
 
         private static (bool valid, List<string> errors) LoadAndValidateConfig(string[] args)
@@ -141,11 +251,19 @@ namespace Portforwarded.Server
             return (!errors.Any(), errors);
         }
 
-        internal static void LogLine(string line, ConsoleColor color = ConsoleColor.White)
+        internal static void LogLine(string line = "", ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(line);
             Console.ForegroundColor = ConsoleColor.White;
         }
+    }
+
+    internal class ForwardMapping
+    {
+        public Open.Nat.Protocol Protocol { get; set; }
+        public string LocalIPAddress { get; set; }
+        public int LocalPort { get; set; }
+        public int PublicPort { get; set; }
     }
 }
