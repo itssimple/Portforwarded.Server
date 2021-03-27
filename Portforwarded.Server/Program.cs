c49495fd8293d4e26f8cb2811d28118e6853e33b
NoLifeKing <webmaster@itssimple.se>
2021-03-27 13:43:45 +0100

    Add project files.

---
+++ Portforwarded.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Minecraft.Portforwarded.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
